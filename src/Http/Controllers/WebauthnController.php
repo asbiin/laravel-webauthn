b1725f173f10fbbcccbf278b4b2ff0a2b4908d19
Alexis Saettler <alexis@saettler.org>
2019-04-14 09:02:35 +0200

    fix callback get

--- src/Http/Controllers/WebauthnController.php
+++ src/Http/Controllers/WebauthnController.php
@@ -118,6 +118,7 @@ class WebauthnController extends Controller
         } elseif (! empty($this->config->get('webauthn.authenticate.postSuccessRedirectRoute'))) {
             return Redirect::intended($this->config->get('webauthn.authenticate.postSuccessRedirectRoute'));
         } else {
+            $callback = $request->session()->pull('url.intended', '/');
             return Response::json([
                 'result' => $result,
                 'callback' => $callback,
