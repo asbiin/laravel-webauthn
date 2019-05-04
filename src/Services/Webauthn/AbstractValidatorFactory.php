2ae9ad7964546df145c6d91e1369a1cf87317fe3
Alexis Saettler <alexis@saettler.org>
2019-05-04 09:18:54 +0200

    Remove Guzzle6 direct dependency

--- src/Services/Webauthn/AbstractValidatorFactory.php
+++ src/Services/Webauthn/AbstractValidatorFactory.php
@@ -51,7 +51,13 @@ abstract class AbstractValidatorFactory extends AbstractFactory
 
         // https://www.w3.org/TR/webauthn/#android-safetynet-attestation
         if ($this->config->get('webauthn.google_safetynet_api_key', '') !== '') {
-            $attestationStatementSupportManager->add(new AndroidSafetyNetAttestationStatementSupport(new Client(), $this->config->get('webauthn.google_safetynet_api_key')));
+            try {
+                $client = \Http\Discovery\HttpClientDiscovery::find();
+                $attestationStatementSupportManager->add(new AndroidSafetyNetAttestationStatementSupport($client, $this->config->get('webauthn.google_safetynet_api_key')));
+            }
+            catch (\Exception\NotFoundException $e) {
+                // ignore
+            }
         }
 
         // https://www.w3.org/TR/webauthn/#android-key-attestation
