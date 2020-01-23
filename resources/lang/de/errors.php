1c749aa0ef20d3103d37a0312075df913437aa5c
Alexis Saettler <alexis@saettler.org>
2020-01-23 23:48:49 +0100

    New translations errors.php (German)

--- resources/lang/de/errors.php
+++ resources/lang/de/errors.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'user_unauthenticated' => 'You need to log in before doing a Webauthn authentication',
-    'auth_data_not_found' => 'Authentication data not found',
-    'create_data_not_found' => 'Register data not found',
-    'object_not_found' => 'Object not found',
+    'user_unauthenticated' => 'Sie müssen sich vor einer WebAuthn-Authentifizierung anmelden',
+    'auth_data_not_found' => 'Authentifizierungsdaten nicht gefunden',
+    'create_data_not_found' => 'Registrierungsdaten nicht gefunden',
+    'object_not_found' => 'Objekt nicht gefunden',
 
-    'not_supported' => 'Your browser doesn’t currently support WebAuthn.',
-    'not_secured' => 'WebAuthn only supports secure connections. For testing over HTTP, you can use the origin "localhost".',
-    'key_already_used' => 'This key is already registered. It’s not necessary to register it again.',
-    'key_not_allowed' => 'The operation either timed out or was not allowed.',
+    'not_supported' => 'Ihr Browser unterstützt noch kein WebAuthn.',
+    'not_secured' => 'WebAuthn unterstützt nur sichere Verbindungen. Für Tests über HTTP können Sie die Herkunft "localhost" verwenden.',
+    'key_already_used' => 'Dieser Sicherheitsschlüssel wurde bereits registriert. Es ist nicht notwendig ihn erneut zu registrieren.',
+    'key_not_allowed' => 'Der Vorgang ist entweder zeitlich abgelaufen oder wurde nicht erlaubt.',
 
 ];
