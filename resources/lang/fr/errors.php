48622fb1df0f0d8c8e65dbb4f1ecd6ea9e69d649
Alexis Saettler <alexis@saettler.org>
2020-03-07 12:15:16 +0100

    New translations errors.php (French)

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -7,8 +7,8 @@ return [
     'object_not_found' => 'Objet introuvable',
 
     'not_supported' => 'Votre navigateur ne prend actuellement pas en charge WebAuthn.',
-    'not_secured' => 'WebAuthn ne prend en charge que les connexions sécurisées. Pour les tests via HTTP, vous pouvez utiliser l\'origine "localhost".',
-    'key_already_used' => 'Cette clé est déjà enregistrée. Il n\'est pas nécessaire de l\'enregistrer à nouveau.',
+    'not_secured' => 'WebAuthn ne prend en charge que les connexions sécurisées. Pour les tests via HTTP, vous pouvez utiliser l’origine "localhost".',
+    'key_already_used' => 'Cette clé est déjà enregistrée. Il n’est pas nécessaire de l’enregistrer à nouveau.',
     'key_not_allowed' => 'L’opération a expiré ou n’a pas été autorisée.',
 
 ];
