e4d0e33b24cde5a8466ffba731ea1a25c990c6f6
Alexis Saettler <alexis@saettler.org>
2023-02-19 18:53:15 +0100

    fix: fix axios integrity in blade views

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,7 +15,7 @@
   <title>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Scripts -->
-  <script src="https://unpkg.com/axios/dist/axios.min.js" integrity="sha384-DodEZXSo26vCefAnqrtuMP+fXrf86iX4o1N718nXYEH/VTnd5BHRsZ5g67mbx3N3" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/axios@1.2.6/dist/axios.min.js" integrity="sha384-TiLmfbX5iPJnd6NZIGGUBlE7UcHkicSKG+1z9u33gDb40R39mnvlpvUlEekwxSd6" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
