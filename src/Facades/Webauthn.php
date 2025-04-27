ab1ebd1067147bd6e7a0b6b0fb0322e9cbfffc02
Alexis Saettler <alexis@saettler.org>
2025-04-27 20:04:58 +0200

    fix: fix facade signature (#502)

--- src/Facades/Webauthn.php
+++ src/Facades/Webauthn.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Illuminate\Database\Eloquent\Model create(\Illuminate\Contracts\Auth\Authenticatable $user, string $keyName, \Webauthn\PublicKeyCredentialSource $publicKeyCredentialSource)
- * @method static void login(\Illuminate\Contracts\Auth\Authenticatable $user)
+ * @method static void login(?\Illuminate\Contracts\Auth\Authenticatable $user)
  * @method static void logout()
  * @method static bool check()
- * @method static \LaravelWebauthn\Services\Webauthn\PublicKeyCredentialRequestOptions prepareAssertion(\Illuminate\Contracts\Auth\Authenticatable $user)
+ * @method static \LaravelWebauthn\Services\Webauthn\PublicKeyCredentialRequestOptions prepareAssertion(?\Illuminate\Contracts\Auth\Authenticatable $user)
  * @method static bool validateAssertion(\Illuminate\Contracts\Auth\Authenticatable $user, array $data)
  * @method static \LaravelWebauthn\Services\Webauthn\PublicKeyCredentialCreationOptions prepareAttestation(\Illuminate\Contracts\Auth\Authenticatable $user)
  * @method static \Illuminate\Database\Eloquent\Model validateAttestation(\Illuminate\Contracts\Auth\Authenticatable $user, array $data, string $keyName)
